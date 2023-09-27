ad51cb764c148fa431e0c3bbbbca3a179acd0f3b
gonasooc <doupler@gmail.com>
2023-09-27 17:43:48 +0900

    Feature: pdf 변환 및 저장 기능 추가 close #5

---
+++ assets/js/custom/pdf.js
@@ -0,0 +1,48 @@
+function downloadPdf() {
+  // .app에 is-dark 클래스 유무 확인
+  const appElement = document.querySelector(".app");
+  const isDarkMode = appElement.classList.contains("is-dark");
+
+  if (isDarkMode) {
+    alert("PDF 저장은 라이트모드에서만 지원합니다.");
+    return;
+  }
+
+  const loadingSpinner = document.getElementById("spinnerWrapper");
+  loadingSpinner.style.display = "flex";
+
+  const divToPrint = document.getElementById("divToPrint");
+
+  html2canvas(divToPrint, {
+    allowTaint: true,
+    useCORS: true,
+    scale: 2,
+  }).then((canvas) => {
+    // 캔버스를 이미지로 변환
+    let imgData = canvas.toDataURL("image/png");
+    let imgWidth = 190; // 이미지 가로 길이(mm) / A4 기준 210mm
+    let pageHeight = imgWidth * 1.414; // 출력 페이지 세로 길이 계산 A4 기준
+    let imgHeight = (canvas.height * imgWidth) / canvas.width;
+    let heightLeft = imgHeight;
+    let margin = 10; // 출력 페이지 여백설정
+    let doc = new jsPDF("p", "mm");
+    let position = 10;
+
+    // 첫 페이지 출력
+    doc.addImage(imgData, "PNG", margin, position, imgWidth, imgHeight);
+    heightLeft -= pageHeight;
+
+    // 한 페이지 이상일 경우 루프 돌면서 출력
+    while (heightLeft >= 20) {
+      position = heightLeft - imgHeight;
+      position = position - 20;
+      doc.addPage();
+      doc.addImage(imgData, "PNG", margin, position, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+    }
+
+    // 파일 저장
+    doc.save("기술이력서_최관수.pdf");
+    loadingSpinner.style.display = "none";
+  });
+}
--- assets/js/custom/ui.js
+++ assets/js/custom/ui.js
@@ -1,11 +1,10 @@
 // 전역 선언 필요한 script
 const $darkModeCheckBox = document.querySelector("#darkModeCheckBox");
 const isUserColorTheme = localStorage.getItem("color-theme");
-const $darkmodeGroup = document.querySelector("#darkmodeGroup");
-const $page = document.querySelector("#page");
+const $fixedBtnWrapper = document.querySelector("#fixedBtnWrapper");
+const $app = document.querySelector("#app");
 const $progressBar = document.querySelector("#progressBar");
 
-let touchStartY = null; // 터치 이벤트 관련 변수
 let isVisible = true; // 다크모드 버튼 관련 변수
 
 window.onload = function () {
@@ -13,11 +12,11 @@ window.onload = function () {
     localStorage.setItem("color-theme", "dark");
     document.documentElement.setAttribute("color-theme", "dark");
     $darkModeCheckBox.setAttribute("checked", true);
-    $page.classList.add("is-dark");
+    $app.classList.add("is-dark");
   } else {
     localStorage.setItem("color-theme", "light");
     document.documentElement.setAttribute("color-theme", "light");
-    $page.classList.remove("is-dark");
+    $app.classList.remove("is-dark");
   }
 };
 
@@ -25,11 +24,11 @@ $darkModeCheckBox.addEventListener("click", (event) => {
   if (event.target.checked) {
     localStorage.setItem("color-theme", "dark");
     document.documentElement.setAttribute("color-theme", "dark");
-    $page.classList.add("is-dark");
+    $app.classList.add("is-dark");
   } else {
     localStorage.setItem("color-theme", "light");
     document.documentElement.setAttribute("color-theme", "light");
-    $page.classList.remove("is-dark");
+    $app.classList.remove("is-dark");
   }
 });
 
@@ -38,13 +37,13 @@ document.addEventListener("wheel", (event) => {
   if (event.deltaY > 0) {
     // 아래로 스크롤
     if (isVisible) {
-      $darkmodeGroup.classList.add("is-hide");
+      $fixedBtnWrapper.classList.add("is-hide");
       isVisible = false;
     }
   } else {
     // 위로 스크롤
     if (!isVisible) {
-      $darkmodeGroup.classList.remove("is-hide");
+      $fixedBtnWrapper.classList.remove("is-hide");
       isVisible = true;
     }
   }
@@ -53,10 +52,10 @@ document.addEventListener("wheel", (event) => {
 // 키보드 이벤트 처리
 document.addEventListener("keydown", (event) => {
   if (event.key === "Home" || event.key === "PageUp") {
-    $darkmodeGroup.classList.remove("is-hide");
+    $fixedBtnWrapper.classList.remove("is-hide");
     isVisible = true;
   } else if (event.key === "End" || event.key === "PageDown") {
-    $darkmodeGroup.classList.add("is-hide");
+    $fixedBtnWrapper.classList.add("is-hide");
     isVisible = false;
   }
 });
@@ -71,39 +70,3 @@ window.addEventListener("scroll", () => {
   // 동적 변경
   $progressBar.style.width = scrollPercentage + "%";
 });
-
-// FIXME: 터치 스크롤 이벤트 추후 조정
-// document.addEventListener("touchstart", (event) => {
-//   // 터치 시작 지점을 기록
-//   touchStartY = event.touches[0].clientY;
-// });
-
-// document.addEventListener("touchmove", (event) => {
-//   if (touchStartY !== null) {
-//     const touchEndY = event.touches[0].clientY;
-//     const deltaY = touchEndY - touchStartY;
-
-//     if (deltaY > 0) {
-//       // 아래로 스크롤
-//       if (isVisible) {
-//         $darkmodeGroup.classList.remove("is-hide");
-//         isVisible = true;
-//       }
-//     } else if (deltaY < 0) {
-//       // 위로 스크롤
-//       if (!isVisible) {
-//         $darkmodeGroup.classList.add("is-hide");
-//         isVisible = false;
-//       }
-//     }
-
-//     // 터치 시작 지점을 업데이트
-//     touchStartY = touchEndY;
-//   }
-// });
-
-// // 터치 종료 이벤트 처리
-// document.addEventListener("touchend", () => {
-//   // 터치 시작 지점 초기화
-//   touchStartY = null;
-// });
